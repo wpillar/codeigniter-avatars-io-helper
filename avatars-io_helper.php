7b570d5328ba96305f5bbd1996e5188b7820f58d
Will Pillar <me@willpillar.com>
2012-06-27 17:59:33 +0100

    Updating documentation

--- avatars-io_helper.php
+++ avatars-io_helper.php
@@ -1,6 +1,38 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 if ( ! function_exists('avatar_url')) {
+    /**
+    * Avatar URL
+    * 
+    * Takes a number of parameters and returns a URL from the Avatars.io service with the respective avatar.
+    *
+    * Usage: 
+    * avatar_url('twitter', 'willpillar')
+    * avatar_url('facebook', 'willpillar');
+    * avatar_url('gravatar', 'me@willpillar.com')
+    *
+    * - Different Sizes
+    * avatar_url('twitter', 'willpillar', 'large')
+    * avatar_url('facebook', 'willpillar', 'medium')
+    *
+    * - Return url instead of echo
+    * avatar_url('twitter', 'willpillar', null, false)
+    *
+    * - Echo an img tag
+    * avatar_url('twitter', 'willpillar', null, true, true)
+    *
+    *
+    * Find out more: http://avatars.io
+    *
+    * @author Will Pillar
+    * @param string
+    * @param string
+    * @param string
+    * @param boolean
+    * @param boolean
+    * @return string
+    * @return void
+    */
     function avatar_url($service, $username_or_id, $size = null, $echo = true, $img_tag = false) {
     	$CI =& get_instance();
     	$CI->load->helper('email');
